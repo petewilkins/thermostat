3fa37b160169db6d0fba954109323ad7bd09f7f1
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-12 11:24:48 +0100

    add power saving checks for maximum tempertaure

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,8 +9,14 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  it('has a default temperature of 20deg', function() {
-    expect(thermostat.currentTemp(0)).toEqual(20);
+  describe('defaults', function() {
+    it('temperature of 20deg', function() {
+      expect(thermostat.currentTemp(0)).toEqual(20);
+    });
+
+    it('power saving ON', function() {
+      expect(thermostat.powerSaving).toEqual(true)
+    });
   });
 
   it('increases temperature', function() {
@@ -29,14 +35,14 @@ describe('Thermostat', function() {
 
   describe('Power saving on', function(){
     it('has a maximum temperature of 25deg', function() {
-      expect(function(){thermostat.up(6);}).toThrowError("Maximum temperature with power saving is 25 degrees!");
+      expect(function(){thermostat.up(6);}).toThrowError("Maximum temperature with power saving ON is 25 degrees!");
     });
   });
 
   describe('Power saving off', function(){
-    it('has a maxmium temperature of 32deg', function(){
-      thermostat.isPowerSaving() = false;
-      expect(function(){thermostat.up(8);}).toThrowError("Maximum temperature without power saving is 32 degrees!");
+    it('has a maximum temperature of 32deg', function() {
+      thermostat.powerSaving = false;
+      expect(function(){thermostat.up(13);}).toThrowError("Maximum temperature with power saving OFF is 32 degrees!");
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,17 +2,19 @@
 
 function Thermostat() {
   this.temperature = 20;
-  this.isPowerSaving = true;
+  this.powerSaving = true;
 }
 
 Thermostat.prototype.currentTemp = function(change) {
-  // return this.tempDefault;
   return this.temperature += change;
 };
 
 Thermostat.prototype.up = function (change) {
-  if(this.isPowerSaving === true && this.temperature+change >= 25){
-    throw new TypeError("Maximum temperature with power saving is 25 degrees!");
+  if(this.powerSaving === true && (this.temperature+change) >= 25){
+    throw new TypeError("Maximum temperature with power saving ON is 25 degrees!");
+  }
+  if(this.powerSaving === false && (this.temperature+change) >= 32){
+    throw new TypeError("Maximum temperature with power saving OFF is 32 degrees!");
   }
   this.currentTemp(change);
 };
@@ -22,8 +24,4 @@ Thermostat.prototype.down = function (change) {
     throw new TypeError("Minimum temperature is 10 degrees!")
   }
   this.currentTemp(change);
-};
-
-Thermostat.prototype.isPowerSaving = function () {
-
 };

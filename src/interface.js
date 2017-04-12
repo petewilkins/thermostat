5544f009c826cb7f381c21f850e390717138a2cf
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-12 16:39:40 +0100

    added coloured backgrounds according to temperature

--- src/interface.js
+++ src/interface.js
@@ -1,21 +1,48 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+  updateTemperature();
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('body').attr('class', thermostat.energyUsage());
+  }
+
   $('#temperature').text(thermostat.temperature);
 
   $('#temperature-up-1').click(function(){
     thermostat.up(1);
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   });
+
   $('#temperature-up-5').click(function(){
     thermostat.up(5);
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   });
+
   $('#temperature-down-1').click(function(){
     thermostat.down(-1);
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   });
+
   $('#temperature-down-5').click(function(){
     thermostat.down(-5);
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   });
+
+  $('#temperature-reset').click(function(){
+    thermostat.resetTemp();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function(){
+    thermostat.powerSavingOn();
+    $('#power-saving-status').text('on')
+    updateTemperature();
+  });
+
+  $('#powersaving-off').click(function(){
+    thermostat.powerSavingOff();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  })
 });
